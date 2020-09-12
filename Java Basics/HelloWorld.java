c0471d5b11aac5046fa5cb39c12a06556f4c41cb
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 14:23:28 +0530

    Update HelloWorld.java

--- Java Basics/HelloWorld.java
+++ Java Basics/HelloWorld.java
@@ -1,1 +1,11 @@
+// This is my first program in java.
+// This program prints Hello World.
 
+class HelloWorld
+{
+  public static void main(String args[])
+  {
+    System.out.println("Hello World");
+  }
+}
+  

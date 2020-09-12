eb13de41f3e016548b44960478f9b14306049085
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 15:13:29 +0530

    Create Addition.java

---
+++ Java Basics/Addition.java
@@ -0,0 +1,12 @@
+// This program shows how to add two numbers in java.
+
+class Addition
+{
+  public static void main(String args[])
+  {
+    int a = 5, b = 10;
+    System.out.println("The addition is : " + (a+b));
+  }
+}
+
+/* Excepted output : The addition is : 15   */

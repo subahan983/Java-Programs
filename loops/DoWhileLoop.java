da22568d8e7e9ea2c539ae0df21fd205395d5eae
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 19:46:57 +0530

    Create DoWhileLoop.java

---
+++ loops/DoWhileLoop.java
@@ -0,0 +1,19 @@
+// This program shows the use of do-while loop in java.
+
+public class DoWhileLoop
+{
+    public static void main(String args[])
+    {
+        int i = 1;
+        
+        do
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+        while(i <= 10);
+        
+    }
+}
+
+/* Expected output : 1 2 3 4 5 6 7 8 9 10    */

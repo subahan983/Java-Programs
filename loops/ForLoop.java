d870bd6be0f54fe49bfc264a924dad919764237a
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 19:41:51 +0530

    Create ForLoop.java

---
+++ loops/ForLoop.java
@@ -0,0 +1,16 @@
+// This program shows the use of for loop in java.
+
+public class ForLoop
+{
+    public static void main(String args[])
+    {
+        int i;
+        
+        for(i = 1; i <= 10; i++)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+/* Expected output : 1 2 3 4 5 6 7 8 9 10    */

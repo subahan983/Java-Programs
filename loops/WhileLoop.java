2a2eaf1a4ce404ece25296d7e90fb22b0b0ac307
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 19:39:38 +0530

    Update WhileLoop.java

--- loops/WhileLoop.java
+++ loops/WhileLoop.java
@@ -1,1 +1,18 @@
+// This program shows how the while loop works in java.
 
+public class WhileLoop
+{
+  public static void main(String args[])
+  {
+    int i = 1;
+    while(i <= 10)
+    {
+      
+      System.out.print(i + " ");
+      i++;
+      
+    }
+  }
+}
+
+/* Excepted output : 1 2 3 4 5 6 7 8 9 10       */

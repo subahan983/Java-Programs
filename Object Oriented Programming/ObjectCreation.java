9de9e32304a1926838639a6757e3fc34d6048192
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 14:35:45 +0530

    Create ObjectCreation.java

---
+++ Object Oriented Programming/ObjectCreation.java
@@ -0,0 +1,21 @@
+// This program shows how to create an object reference to a class in java.
+
+class Another
+{
+  void hello()
+  {
+    System.out.println("Hello I am in new Class");
+  }
+}
+
+
+class ObjectCreation
+{
+  public static void main(String args[])
+  {
+    Another object = new Another();  // Creating an object reference.
+    object.hello();
+  }
+}
+
+/* Excepted Output : Hello I am in new Class */
